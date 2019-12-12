fc029d5c601bd46f544fbc033c6ce9f0f1a017b0
Michael Chen <mechiland@gmail.com>
2019-12-12 18:44:08 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const team = [
+  { id: 'xqzhou', name: "周晓强", email: "xqzhou@hipacloud.com"},
+  { id: 'mchen', name: "陈金洲", email: "xqzhou@hipacloud.com"},
+  { id: 'yzhuang', name: "黄一钊", email: "xqzhou@hipacloud.com"},
+  { id: 'byliu', name: "刘丙乙", email: "xqzhou@hipacloud.com"},
+]
+
+const Users = ({users}) => {
+
+  return <ul>
+    {users.forEach(u => {
+      return <li>{u.name}</li>
+    })}
+  </ul>
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>团队列表</h1>
+      <Users users={team}/>
     </div>
   );
 }
