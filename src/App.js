b03a3f27f44f376e24f987f4b941ae15e0343607
Michael Chen <mechiland@gmail.com>
2019-12-12 18:46:45 +0800

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const team = [
-  { id: 'xqzhou', name: "周晓强", email: "xqzhou@hipacloud.com"},
-  { id: 'mchen', name: "陈金洲", email: "xqzhou@hipacloud.com"},
-  { id: 'yzhuang', name: "黄一钊", email: "xqzhou@hipacloud.com"},
-  { id: 'byliu', name: "刘丙乙", email: "xqzhou@hipacloud.com"},
-]
+  { id: "xqzhou", name: "周晓强", email: "xqzhou@hipacloud.com" },
+  { id: "mchen", name: "陈金洲", email: "xqzhou@hipacloud.com" },
+  { id: "yzhuang", name: "黄一钊", email: "xqzhou@hipacloud.com" },
+  { id: "byliu", name: "刘丙乙", email: "xqzhou@hipacloud.com" }
+];
 
-const Users = ({users}) => {
-
-  return <ul>
-    {users.forEach(u => {
-      return <li>{u.name}</li>
-    })}
-  </ul>
-}
+const Users = ({ users }) => {
+  return (
+    <ul>
+      {users.map(u => {
+        return <li>{u.name}</li>;
+      })}
+    </ul>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <h1>团队列表</h1>
-      <Users users={team}/>
+      <Users users={team} />
     </div>
   );
 }
